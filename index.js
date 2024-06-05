b780b72bc0fcbb7ddc018b762cfdfbd71c359f7a
Johnson1123 <onifadejohnson2017@gmail.com>
2024-06-05 04:37:04 -0700

    adding seperate route for hotels, cars, dogs, phones, mall

--- index.js
+++ index.js
@@ -16,13 +16,49 @@ import Cars from "./data/car.js";
 import mall from "./data/product.js";
 import dogs from "./data/dogs.js";
 import phone from "./data/phone.js";
+import hotel from "./data/hotel.js";
 
 const app = express();
 
-const allProduct = [...Cars, ...mall, ...dogs, ...phone];
+const allProduct = [...Cars, ...mall, ...dogs, ...phone, ...hotel];
 
 app.get("/", (req, res) => {
   res.send(allProduct);
+});
+app.get("/mall", (req, res) => {
+  res.json({
+    success: true,
+    category: "Mall",
+    data: mall,
+  });
+});
+app.get("/phones", (req, res) => {
+  res.json({
+    success: true,
+    category: "Phones",
+    data: phone,
+  });
+});
+app.get("/cars", (req, res) => {
+  res.json({
+    success: true,
+    category: "car",
+    data: Cars,
+  });
+});
+app.get("/dogs", (req, res) => {
+  res.json({
+    success: true,
+    category: "dog",
+    data: dogs,
+  });
+});
+app.get("/hotels", (req, res) => {
+  res.json({
+    success: true,
+    category: "hotel",
+    data: hotel,
+  });
 });
 
 app.listen(4000, () => console.log("server is running in port 4000"));
